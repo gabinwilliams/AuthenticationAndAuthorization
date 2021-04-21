f5dd3dea3d262e71cbd46e9cad9876308f54bd12
gabinwilliams <scoutsnknives@gmail.com>
2021-04-21 13:58:00 -0500

    updating master to switch back to branch

---
+++ src/components/HomeCard/HomeCard.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import TinderCard from 'react-tinder-card';
+import './HomeCard.css';
+
+const HomeCard = () => {
+
+  const [people, setPeople] = useState([
+
+    {
+      name: 'Steve Jobs',
+      url: 'https://hs-marketing.imgix.net/images/blog/steve-jobs.jpg?auto=compress&fit=scale&w=1200&h=673&dpr=1',
+    },
+    {
+      name: 'batman',
+      url: 'https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fbatman-arkham-knight%2FEGS_WB_Batman_Arkham_Knight_G1_1920x1080_19_0911-1920x1080-1d69e15f00cb5ab57249f208f1f8f45d52cbbc59.jpg?h=1080&resize=1&w=1920',
+    }
+
+  ]);
+
+  return (
+
+    <div>
+      <h1>tinderCards</h1>
+      {people.map(person => (
+        <TinderCard 
+          key={person.name}
+          className="swipe"
+          // prop from tinder-card library to prevent swiping up or down
+          preventSwipe={['up', 'down']}
+        >
+          <div 
+          className='card'
+          style={{backgroundImage: `url(${person.url})`}}
+          >
+            <h3>{person.name}</h3>
+          </div>
+        </TinderCard> 
+      ))}
+    </div>
+  );
+};
+
+export default HomeCard;
