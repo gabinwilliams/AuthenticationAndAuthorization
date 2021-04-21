bdd47fded844d96e460caceca56ec26fc645d551
gabinwilliams <scoutsnknives@gmail.com>
2021-04-21 13:58:00 -0500

    updating master to switch back to branch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import "./Home.css";
 import HomeHeader from "../HomeHeader/HomeHeader";
+import HomeCard from "../HomeCard/HomeCard";
 
 const Home = () => {
   return (
     <div>
       <HomeHeader />
-      {/* Tinder Cards */}
+      <HomeCard />
       {/* Buttons below tinder card */}
     </div>
   );
