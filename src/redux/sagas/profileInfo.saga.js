d178d52f829ae27b98ea138c4dbf2e9ad6bd0db0
gabinwilliams <scoutsnknives@gmail.com>
2021-04-24 22:25:56 -0500

    completed PUT ROUTE for ProfileEdit page

--- src/redux/sagas/profileInfo.saga.js
+++ src/redux/sagas/profileInfo.saga.js
@@ -14,6 +14,8 @@ function* registerUserInfo(action) {
     // yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
     // passes the username and password from the payload to the server
     yield axios.post('/api/user/registerInfo', action.payload);
+    
+    yield put({ type: 'FETCH_USER' });
   } catch (error) {
     console.log('Error with user registration:', error);
     yield put({ type: 'REGISTRATION_FAILED' });
