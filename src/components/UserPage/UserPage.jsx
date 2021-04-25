7e3dd27ad02e161074d365c43fc4f17c4a59ddfa
gabinwilliams <scoutsnknives@gmail.com>
2021-04-25 14:06:38 -0500

    added styling to ProfileForm

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import ProfileForm from '../ProfileForm/ProfileForm';
+import './UserPage.css';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <h4>Let's fill out your profile information so you can start connecting with other developers!</h4>
+      <div className="hero">
+        <h2>Welcome, {user.username}!</h2>
+        <h4>Let's fill out your profile information so you can start connecting with other developers!</h4>
+      </div>
       <ProfileForm />
       {/* <LogOutButton className="btn" /> */}
     </div>
