6f6e9bb34e75f2c80776d42bf9658f0813a8e69c
gabinwilliams <scoutsnknives@gmail.com>
2021-04-28 18:20:03 -0500

    Messages page layout and styling

---
+++ src/components/MessagesInput/MessagesInput.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'sticky',
+    bottom: 0,
+    background: 'rgba(220, 220, 220, .8)',
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    borderRadius: 5,
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: '75vw',
+  },
+  chatField: {
+    
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  }
+}));
+
+export default function LayoutTextFields() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      
+       
+      <div className={classes.chatField}>
+
+      
+      
+        <TextField
+            label="Chat"
+            id="outlined-margin-normal"
+            // defaultValue="Default Value"
+            className={classes.textField}
+            // helperText="Some important text"
+            margin="normal"
+            variant="outlined"
+          />
+          <IconButton>
+            <SendIcon color='primary' fontSize="large"></SendIcon>
+          </IconButton>
+        </div>
+      
+    </div>
+  );
+}
