bc9c413deeb4ff80c781a68dc6746b63a326673d
gabinwilliams <scoutsnknives@gmail.com>
2021-05-07 17:07:35 -0500

    removed un-used images

--- database.sql
+++ database.sql
@@ -47,34 +47,34 @@ CREATE TABLE "current_chat" (
 );
 
 
--- BIG SORTING QUERY to make sure profiles that the user has liked, will not show up in the users feed
+-- BIG SORTING QUERY to make sure profiles that the user has liked, will not show up in the users fee
 
-    "match" BOOLEAN DEFAULT FALSE
-    
-);
-
--- GET ALL USERS WITH "liked" = false
-SELECT u.* -- Returns all columns from user table
-FROM "user" as u -- Selects from user table, aliased as u
-JOIN "user_likes" as likes ON u.id = likes.liked_user_id -- JOINS only LIKED USER
-WHERE likes.liked = false OR -- Get everyone who wasn't "liked"
-      likes.liked_user_id IS NULL OR -- Get everyone who isn't in the "user_likes" table
-      u.id != 5 -- Exclude the loged-in user
-GROUP BY u.id;
-
--- build list of users with liked = false
-WITH unchosen as (
-    SELECT u.*
-    FROM "user" as u
-    JOIN "user_likes" as liked ON liked.liked_user_id = u.id
-    WHERE liked.liked = false AND liked.user_id = 12
-    GROUP BY u.id
--- ), no_decision as ( -- build list of users no in user_liked table
---     SELECT u.*
---     FROM "user" as u
---     JOIN "user_likes" as liked ON liked.liked_user_id = u.id
---     WHERE liked.liked_user_id IS NULL and liked.user_id = 12
---     GROUP BY u.id
+WITH unchosen as ( -- Create the variable and name it
+    SELECT u.* -- Return every column from "user", aliased as u
+    FROM "user" as u -- Add "user" table, alias as u
+    JOIN "user_likes" as liked ON liked.liked_user_id = u.id -- JOIN "user_likes" aliased as liked
+    WHERE liked.liked = false AND liked.user_id = $1 -- Replace 12 with req.user.id / $1
+    -- filter by liked=false, not authenticated user.
+    GROUP BY u.id -- returns distinct user ids.
 )
+
+-- Select all columns from user;
+SELECT u.*
+-- Add user table, give alias of u since user is a reserved word in Postgres
+FROM "user" as u
+-- Ensure each user in the unchosen query above is returned
+WHERE u.id IN (SELECT id FROM unchosen) OR 
+    -- FILTER OUT users in the next query
+    u.id NOT IN (
+        -- Select everyone that has a "liked"=true and "liked" by the authenticated user
+        SELECT "user_likes".liked_user_id
+        from "user_likes"
+        WHERE "user_likes".liked = true AND "user_likes".user_id = $1
+        -- Group by liked_user_id to remove duplicate entries
+        -- WHY? Saves time returning, because this is a small list
+        -- It's easier to GROUP/SORT/COUNT/WHATEVER a small group
+        -- Than do the same action on a very large table
+        --GROUP BY "user_likes".liked_user_id
+    );
 
 
