33049eba246104f56be3e83dc9bdee7599a4e000
gabinwilliams <scoutsnknives@gmail.com>
2021-05-07 17:17:59 -0500

    added rejectUnauthenticated to all routes in cards router

--- server/routes/cards.router.js
+++ server/routes/cards.router.js
@@ -1,21 +1,16 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 const {
   rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
-const userStrategy = require('../strategies/user.strategy');
+} = require("../modules/authentication-middleware");
+const encryptLib = require("../modules/encryption");
+const userStrategy = require("../strategies/user.strategy");
 
 /**
  * GET route template
 //  */
-router.get('/', (req, res) => {
-
-
-  // const query = `SELECT * FROM "user"
-  // WHERE "id" != ${req.user.id};
-  // ;`
+router.get("/", rejectUnauthenticated, (req, res) => {
   const query = ` WITH unchosen as ( -- Create the variable and name it
     SELECT u.* -- Return every column from "user", aliased as u
     FROM "user" as u -- Add "user" table, alias as u
@@ -43,91 +38,42 @@ WHERE u.id IN (SELECT id FROM unchosen) OR
         -- Than do the same action on a very large table
         --GROUP BY "user_likes".liked_user_id
     );
-  `
+  `;
 
-  pool.query(query, [req.user.id])
-    .then( result => {
-     
+  pool
+    .query(query, [req.user.id])
+    .then((result) => {
       res.send(result.rows);
     })
-    .catch(err => {
-      console.log('ERROR: Get all profiles', err);
-      res.sendStatus(500)
-    })
+    .catch((err) => {
+      console.log("ERROR: Get all profiles", err);
+      res.sendStatus(500);
+    });
 });
 
-// router.get('/filteredProfiles', (req, res) => {
-
-
-//   const query = ` SELECT "user".name, "user".id AS user_id, "user".active, "user".bio, "user".dev_type, "user".github, "user".profile_image, "user".tech_one, "user".tech_two, 	"user".tech_three, "user".username
-//   FROM "user"
-//   LEFT JOIN (
-// --   Give me all users that have swiped left or right
-//   SELECT "user".id
-//   FROM "user"
-//   JOIN user_likes ON "user".id = user_likes.user_id
-//   WHERE user_likes.user_id = "user".id
-//   GROUP BY "user".id 
-  
-//   UNION
-// --  Give me all users that have been swiped on left or right
-//   SELECT "user".id
-//   FROM "user"
-//   JOIN user_likes ON "user".id = user_likes.liked_user_id
-//   WHERE user_likes.liked_user_id = "user".id 
-//   GROUP BY "user".id 
-  
-//   ) AS "userlist" ON "userlist".id = "user".id
-// --  I want all users who haven't swiped or been swiped could change to
-//   WHERE "userlist".id IS NULL AND "user".id != $1
-//   GROUP BY "user".id;
-//   ;`
-
-//   pool.query(query, [req.user.id])
-//     .then( result => {
-     
-//       res.send(result.rows);
-//     })
-//     .catch(err => {
-//       console.log('ERROR: Get all profiles', err);
-//       res.sendStatus(500)
-//     })
-// });
-
-
-
-router.get('/likes', (req, res) => {
-
+router.get("/likes", rejectUnauthenticated, (req, res) => {
   const query = `SELECT "user".name, "user".id AS user_id, "user".active, "user".bio, "user".dev_type, "user".github, "user".profile_image, "user".tech_one, "user".tech_two, "user".tech_three, "user".username, "user_likes".liked, "user_likes".liked_user_id, "user_likes".match
   FROM "user_likes"
   JOIN "user" ON "user".id = "user_likes".user_id
   ORDER BY "match" DESC
   ;`;
 
-  
-
-
-  pool.query(query)
-    .then( result => {
-     
+  pool
+    .query(query)
+    .then((result) => {
       res.send(result.rows);
     })
-    .catch(err => {
-      console.log('ERROR: Get all profiles', err);
-      res.sendStatus(500)
-    })
+    .catch((err) => {
+      console.log("ERROR: Get all profiles", err);
+      res.sendStatus(500);
+    });
 });
 
 /**
  * POST route template
  */
-router.put('/match', (req, res) => {
-  // POST route code here
-  // const queryText = `UPDATE "user" 
-  // SET "name" = $1, dev_type = $2, profile_image = $3, bio = $4, github = $5, tech_one = $6, tech_two = $7, tech_three = $8, active = $9
-  // WHERE "user".id = ${req.body.id}
-
-  console.log('In POST /match', req.body);
+router.put("/match", rejectUnauthenticated, (req, res) => {
+  console.log("In POST /match", req.body);
   const queryText = `UPDATE "user_likes" 
     SET "match" = $1
     WHERE "user_id" = ${req.body.user_id} AND "liked_user_id" = ${req.body.liked_user_id} OR "user_id" = ${req.body.liked_user_id} AND "liked_user_id" = ${req.body.user_id}
@@ -136,16 +82,13 @@ router.put('/match', (req, res) => {
     .query(queryText, [req.body.match])
     .then(() => res.sendStatus(201))
     .catch((err) => {
-      console.log('error in PUT /match ', err);
+      console.log("error in PUT /match ", err);
       res.sendStatus(500);
     });
 });
 
-
-router.delete('/connection/request/:id', (req, res) => {
- 
-
-  console.log('In DELETE /connection/request', req.params);
+router.delete("/connection/request/:id", rejectUnauthenticated, (req, res) => {
+  console.log("In DELETE /connection/request", req.params);
   const queryText = `
   DELETE FROM "user_likes" 
   WHERE "user_likes".user_id = ${req.params.id}
@@ -154,10 +97,9 @@ router.delete('/connection/request/:id', (req, res) => {
     .query(queryText)
     .then(() => res.sendStatus(201))
     .catch((err) => {
-      console.log('error in DELETE /connection/request ', err);
+      console.log("error in DELETE /connection/request ", err);
       res.sendStatus(500);
     });
 });
-
 
 module.exports = router;
