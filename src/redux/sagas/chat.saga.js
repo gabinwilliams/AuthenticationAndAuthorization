7c22f20e7bd07f767ed4b1d65924577a992e4523
gabinwilliams <78612204+gabinwilliams@users.noreply.github.com>
2022-09-14 04:21:11 -0400

    Clean up sagas (#32)

--- src/redux/sagas/chat.saga.js
+++ src/redux/sagas/chat.saga.js
@@ -1,12 +1,8 @@
 import { put, takeEvery, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-// worker Saga: will be fired on "REGISTER" actions
 function* fetchCurrentChat() {
   try {
-    // clear any existing error on the registration page
-    // yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
-    // passes the username and password from the payload to the server
     const response = yield axios.get("/api/chat/currentChat");
     console.log("GET currentChat:", response.data);
     yield put({ type: "CURRENT_CHAT", payload: response.data });
@@ -17,9 +13,6 @@ function* fetchCurrentChat() {
 
 function* fetchMessages() {
   try {
-    // clear any existing error on the registration page
-    // yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
-    // passes the username and password from the payload to the server
     const response = yield axios.get("/api/chat/messages");
     console.log("GET for FETCH_MESSAGES: ", response.data);
     yield put({ type: "UPDATE_MESSAGES", payload: response.data });
--- src/redux/sagas/fetchCards.saga.js
+++ src/redux/sagas/fetchCards.saga.js
@@ -1,12 +1,8 @@
 import { put, takeEvery, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-// worker Saga: will be fired on "REGISTER" actions
 function* getUserProfiles() {
   try {
-    // clear any existing error on the registration page
-    // yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
-    // passes the username and password from the payload to the server
     const profiles = yield axios.get("/api/profile");
     console.log("GET if getting:", profiles.data);
     yield put({ type: "FETCH_PROFILES", payload: profiles.data });
@@ -17,9 +13,6 @@ function* getUserProfiles() {
 
 function* fetchUserLikes() {
   try {
-    // clear any existing error on the registration page
-    // yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
-    // passes the username and password from the payload to the server
     const userLikes = yield axios.get("/api/profile/likes");
     console.log("GET if getting:", userLikes.data);
     yield put({ type: "FETCH_USER_LIKES", payload: userLikes.data });
